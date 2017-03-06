85cab5c35c84f1a89db71af7e8b370947d3f4cf7
Neeraj Sarna <sarna@turbine.mathcces.rwth-aachen.de>
2017-03-06 15:16:50 +0100

    added all results for G35 and G26

--- build/CMakeFiles/dg_moments.out.dir/cmake_clean.cmake
+++ build/CMakeFiles/dg_moments.out.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/dg_moments.out.dir/main.cc.o"
   "dg_moments.out.pdb"
   "dg_moments.out"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/dg_moments.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/dg_moments.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
